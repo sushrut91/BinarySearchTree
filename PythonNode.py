e3a9de54556eb037336f1593aa8c13764a8f5b7b
Sushrut <sushrut91@gmail.com>
2016-08-13 16:55:45 -0400

    Separation of earlier logic into Node and tree classes

--- PythonNode.py
+++ PythonNode.py
@@ -1,18 +1,33 @@
-class BinaryTree:
-	def __init__(self):
-		self.data = None
-		self.left = None
-		self.right = None
-	def printNode(self):
-		print self.data
-	def insertNode(self,data):
-		if self.left == None and self.right == None:
-			self.data = data
-			self.left = data
-			self.right = data
-		elif data > self.data:
-			self.data = data
-			self.right = data
+class Node:
+	def __init__(self,val):
+		self.value = val
+		self.leftChild = None
+		self.rightChild = None
+		
+	def insert(self,data):
+		if self.value == data:
+			return False
+		elif data < self.value:
+			#Check if there is a left child already present
+			if self.leftChild:
+				return self.left.insert(data)
+			else:
+				self.leftChild = Node(data)
+				return True
 		else:
-			self.data = data
-			self.left = data				
+			if self.rightChild:
+				self.rightChild.insert(data)
+			else:
+				self.rightChild = Node(data)
+class Tree:
+
+	def __init__(self):
+		self.root = None
+		
+	def insert(self,data):
+		if self.root == None:
+			return self.root.insertNode(data)
+		else:
+			self.root = Node(data)
+			'''The node wass added hence return True'''
+			return True
